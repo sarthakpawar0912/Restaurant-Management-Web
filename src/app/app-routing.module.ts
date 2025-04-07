c1626d10a1626f2ddf697e7a25389c79cf4ec33f
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-07 19:54:35 +0530

    Updates mad in projects also bugs are fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { SignupComponent } from './auth-component/signup/signup.component';
 import { LoginComponent } from './auth-component/login/login.component';
 
 const routes: Routes = [
+
   { path: 'signup', component:SignupComponent},
   { path: '', component:LoginComponent},
   { path: 'admin', loadChildren:()=>import("./modules/admin/admin.module").then(m=>m.AdminModule)},
   { path: 'customer', loadChildren:()=>import("./modules/customer/customer.module").then(m=>m.CustomerModule)},
- ];
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
