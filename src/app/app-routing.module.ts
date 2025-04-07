579646c9b5105a73dd8c364e4d9c5de898ca5304
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-07 19:45:27 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,11 @@ const routes: Routes = [
   { path: '', component:LoginComponent},
   { path: 'admin', loadChildren:()=>import("./modules/admin/admin.module").then(m=>m.AdminModule)},
   { path: 'customer', loadChildren:()=>import("./modules/customer/customer.module").then(m=>m.CustomerModule)},
- 
  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
